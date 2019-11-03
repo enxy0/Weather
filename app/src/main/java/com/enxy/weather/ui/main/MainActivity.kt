356035a1fd38695bd5fd97d34bd922b16b10bcd9
enxy0 <enxyox@gmail.com>
2019-11-04 00:18:06 +0300

    Add Bottom App Bar.

--- app/src/main/java/com/enxy/weather/ui/main/MainActivity.kt
+++ app/src/main/java/com/enxy/weather/ui/main/MainActivity.kt
@@ -1,18 +1,37 @@
 package com.enxy.weather.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import com.enxy.weather.R
+import com.enxy.weather.ui.search.SearchActivity
+import kotlinx.android.synthetic.main.main_activity.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
+        setSupportActionBar(bottomAppBar)
+        bottomAppBar.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.menu_search_action -> {
+                    startActivity(Intent(this, SearchActivity::class.java))
+                    true
+                }
+                else -> false
+            }
+        }
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
                 .replace(R.id.mainContainer, MainFragment.newInstance())
                 .commitNow()
         }
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.main_menu, menu)
+        return true
     }
 }
