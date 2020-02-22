c58529e3e550cdbd4507934b74121e3437999fc5
enxy0 <enxyox@gmail.com>
2020-02-23 01:07:11 +0300

    Populate SettingsFragment with data (app info, units)
    Signed-off-by: enxy0 <enxyox@gmail.com>

--- app/src/main/java/com/enxy/weather/ui/settings/SettingsFragment.kt
+++ app/src/main/java/com/enxy/weather/ui/settings/SettingsFragment.kt
@@ -1,7 +1,14 @@
 package com.enxy.weather.ui.settings
 
+import android.content.Intent
+import android.net.Uri
+import android.os.Bundle
+import android.view.View
+import com.enxy.weather.BuildConfig
 import com.enxy.weather.R
 import com.enxy.weather.base.BaseFragment
+import com.enxy.weather.ui.MainActivity
+import kotlinx.android.synthetic.main.settings_fragment.*
 
 class SettingsFragment : BaseFragment() {
     override val layoutId = R.layout.settings_fragment
@@ -9,5 +16,16 @@ class SettingsFragment : BaseFragment() {
     companion object {
         const val TAG = "SettingsFragment"
         fun newInstance() = SettingsFragment()
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        appVersion.text = BuildConfig.VERSION_NAME
+        buildNumber.text = BuildConfig.VERSION_CODE.toString()
+        val openGithub =
+            Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.settings_summary_github)))
+        val openVk = Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.settings_vk_link)))
+        githubLayout.setOnClickListener { (activity as MainActivity).startActivity(openGithub) }
+        authorLayout.setOnClickListener { (activity as MainActivity).startActivity(openVk) }
     }
 }
