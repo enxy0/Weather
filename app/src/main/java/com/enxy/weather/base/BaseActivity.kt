c65f143ff0ce4650256e85e6c59823b0b7f50162
enxy0 <enxyox@gmail.com>
2020-02-23 21:53:39 +0300

    Fix appComponent NPE in BaseActivity
    Signed-off-by: enxy0 <enxyox@gmail.com>

--- app/src/main/java/com/enxy/weather/base/BaseActivity.kt
+++ app/src/main/java/com/enxy/weather/base/BaseActivity.kt
@@ -3,10 +3,13 @@ package com.enxy.weather.base
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.ViewModelProvider
 import com.enxy.weather.AndroidApplication
+import com.enxy.weather.di.ApplicationComponent
 import com.enxy.weather.ui.MainViewModel
 
 abstract class BaseActivity : AppCompatActivity() {
-    val appComponent = (application as AndroidApplication).appComponent
+    val appComponent: ApplicationComponent by lazy {
+        (application as AndroidApplication).appComponent
+    }
 
     fun getViewModel(activity: BaseActivity, factory: ViewModelProvider.Factory) =
         ViewModelProvider(activity, factory).get(MainViewModel::class.java)
