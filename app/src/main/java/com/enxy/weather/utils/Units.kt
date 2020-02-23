8f811f63412e3f0a3b24b59fa0dff63f166f24bb
enxy0 <enxyox@gmail.com>
2020-02-23 21:23:53 +0300

    Add units converter functions
    Signed-off-by: enxy0 <enxyox@gmail.com>

---
+++ app/src/main/java/com/enxy/weather/utils/Units.kt
@@ -0,0 +1,14 @@
+package com.enxy.weather.utils
+
+import kotlin.math.roundToInt
+
+private const val millimeterOfMercury: Double = 133.3223684
+
+fun mmHgToHectoPascals(mmHg: Int): Int = (mmHg / millimeterOfMercury * 100).roundToInt()
+
+fun hectoPascalsToMmHg(hectoPascals: Int): Int =
+    (hectoPascals / 100 * millimeterOfMercury).roundToInt()
+
+fun celsiusToFahrenheit(celsius: Int): Int = (celsius * 1.8 + 32).roundToInt()
+
+fun fahrenheitToCelsius(fahrenheit: Int): Int = ((fahrenheit - 32) / 1.8).roundToInt()
