48afd0a8e60512046c3d950e4784e1c6c296edad
enxy0 <enxyox@gmail.com>
2020-02-23 21:48:45 +0300

    Refactor MainActivity
    Signed-off-by: enxy0 <enxyox@gmail.com>

--- app/src/main/java/com/enxy/weather/ui/MainActivity.kt
+++ app/src/main/java/com/enxy/weather/ui/MainActivity.kt
@@ -1,26 +1,29 @@
 package com.enxy.weather.ui
 
 import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.commitNow
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.lifecycleScope
-import com.enxy.weather.AndroidApplication
 import com.enxy.weather.R
+import com.enxy.weather.base.BaseActivity
 import com.enxy.weather.ui.main.MainFragment
 import com.enxy.weather.ui.search.SearchFragment
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
     @Inject lateinit var viewModelFactory: ViewModelProvider.Factory
     private lateinit var viewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        (application as AndroidApplication).appComponent.inject(this)
-        viewModel = ViewModelProvider(this, viewModelFactory)[MainViewModel::class.java]
+        appComponent.inject(this)
+        viewModel = getViewModel(this, viewModelFactory)
         setContentView(R.layout.main_activity)
+        showFragment(savedInstanceState)
+    }
+
+    private fun showFragment(savedInstanceState: Bundle?) {
         lifecycleScope.launch {
             if (savedInstanceState == null) {
                 if (viewModel.isAppFirstLaunched())
