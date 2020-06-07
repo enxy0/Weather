efb89e408b49e346aadb0908e130959dbdc32091
enxy0 <enxyox@gmail.com>
2020-06-08 00:49:17 +0300

    Add extension function to open links from Fragment

---
+++ app/src/main/java/com/enxy/weather/utils/extension/Fragment.kt
@@ -0,0 +1,16 @@
+package com.enxy.weather.utils.extension
+
+import android.content.Intent
+import android.net.Uri
+import androidx.annotation.StringRes
+import androidx.fragment.app.Fragment
+
+fun Fragment.openLink(@StringRes resId: Int) {
+    val link = Intent(Intent.ACTION_VIEW, Uri.parse(getString(resId)))
+    startActivity(link)
+}
+
+fun Fragment.openLink(url: String) {
+    val link = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+    startActivity(link)
+}
