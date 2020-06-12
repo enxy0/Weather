ab536cf5d40fc73b39418f3a12ffc5cf7e970e01
enxy0 <enxyox@gmail.com>
2020-06-12 10:28:20 +0300

    Move runDelayed to Lifecycle.kt

--- app/src/main/java/com/enxy/weather/utils/extension/Lifecycle.kt
+++ app/src/main/java/com/enxy/weather/utils/extension/Lifecycle.kt
@@ -3,10 +3,23 @@ package com.enxy.weather.utils.extension
 import androidx.lifecycle.LifecycleOwner
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Observer
+import androidx.lifecycle.lifecycleScope
 import com.enxy.weather.utils.exception.Failure
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 fun <T : Any, L : LiveData<T>> LifecycleOwner.observe(liveData: L, body: (T?) -> Unit) =
     liveData.observe(this, Observer(body))
 
 fun <L : LiveData<Failure>> LifecycleOwner.failure(liveData: L, body: (Failure?) -> Unit) =
     liveData.observe(this, Observer(body))
+
+/**
+ * Runs (invokes) function after the delay
+ */
+inline fun LifecycleOwner.runDelayed(delayTime: Long, crossinline fn: () -> Unit) {
+    lifecycleScope.launch {
+        delay(delayTime)
+        fn()
+    }
+}
