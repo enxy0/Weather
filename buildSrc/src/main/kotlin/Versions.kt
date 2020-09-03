a6841d00545c4a2299b213db76c84dd459b04161
enxy0 <enxyox@gmail.com>
2020-09-03 21:50:11 +0300

    :elephant: Gradle: Update app version to 1.0.2-release

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -5,8 +5,8 @@ object Versions {
     const val MIN_SDK = 23
 
     // App version
-    const val VERSION_CODE = 184 // number of commits
-    const val VERSION_NAME = "1.0.1-release"
+    const val VERSION_CODE = 190 // number of commits
+    const val VERSION_NAME = "1.0.2-release"
 
     // Kotlin
     const val KOTLIN = "1.4.0"
